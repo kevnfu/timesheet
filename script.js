5c907c1952190e69480bf4d87fa981720b61f4f1
Kevin <kevn.fu@gmail.com>
2018-04-03 13:16:46 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDtO3sD-Wjaa5BPVDjVjnFmgcRKb9uGZPA",
+    authDomain: "timesheet-15465.firebaseapp.com",
+    databaseURL: "https://timesheet-15465.firebaseio.com",
+    projectId: "timesheet-15465",
+    storageBucket: "timesheet-15465.appspot.com",
+    messagingSenderId: "935580732376"
+};
+firebase.initializeApp(config);
+
+let db = firebase.database();
+let ref = db.ref("data");
+
+$(document).on("click", "button[type='submit']", function() {
+    ref.push({
+        name: $("#name-input").val(), 
+        role: $("#role-input").val(), 
+        date: $("#date-input").val(),
+        rate: $("#rate-input").val()
+    });
+})
+
+$(document).ready(function() {
+
+});
+
+
+ref.on("child_added", function(snapshot, prevChildKey) {
+    console.log(snapshot.val());
+    $("#table-body").append($("<tr>")
+        .append($("<td>").html(snapshot.val().name))
+        .append($("<td>").html(snapshot.val().role))
+        .append($("<td>").html(snapshot.val().date))
+        .append($("<td>").html("coming soon"))
+        .append($("<td>").html(snapshot.val().rate))
+        .append($("<td>").html("coming soon"))
+    );
+});
